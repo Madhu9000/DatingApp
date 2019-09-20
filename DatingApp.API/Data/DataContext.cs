86aa6bea8fdb33619118bc7a6d36b5065feef68c
Madhu9000 <podapati.madhunaidu@gmail.com>
2019-09-20 15:44:56 +0530

    Module-11 : File Upload partially completed

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -3,13 +3,14 @@ using DatingApp.API.Model;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> optiona):base(optiona)
+        public DataContext(DbContextOptions<DataContext> optiona) : base(optiona)
         {
-            
+
         }
-       public DbSet<Value> Values {set; get;}
-       public DbSet<User> Users {set; get;}
+        public DbSet<Value> Values { set; get; }
+        public DbSet<User> Users { set; get; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -5,9 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DatingRepository: IDatingRepository
+    public class DatingRepository : IDatingRepository
     {
-         private readonly DataContext _context;
+        private readonly DataContext _context;
         public DatingRepository(DataContext context)
         {
             _context = context;
@@ -24,16 +24,23 @@ namespace DatingApp.API.Data
         public async Task<User> GetUser(int id)
         {
             var user = await _context.Users.Include(p => p.Photos)
-            .FirstOrDefaultAsync(u => u.Id == id );
+            .FirstOrDefaultAsync(u => u.Id == id);
 
             return user;
         }
-         public async Task<IEnumerable<User>> GetUsers(){
+        public async Task<Photo> GetPhoto(int id)
+        {
+            var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
 
-              var users = await _context.Users
-              .Include(p => p.Photos).ToListAsync();
-              return users;
-         }
+            return photo;
+        }
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+
+            var users = await _context.Users
+            .Include(p => p.Photos).ToListAsync();
+            return users;
+        }
         public async Task<bool> SaveAll()
         {
             return await _context.SaveChangesAsync() > 0;
